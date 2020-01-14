4bc5a6647aec56ac7c7cdef67f8c43b104e2a5a4
Jose Sandino <josesandinonio@gmail.com>
2020-01-13 21:27:03 -0500

    130120

--- primer/example1.js
+++ primer/example1.js
@@ -1,11 +1,39 @@
 class AppComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {count: 5, title: "Hello World"};
+        this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this);
+    }
+
     render() {
+        console.log("RENDERING");
+        const {count, title} = this.state;
+
         return (
             <section className="site-wrap">
-                <h1>Header</h1>
-                <p>Lorum</p>
+                <h1>Header: {title}!</h1>
+                <p>The counter is: {count}</p>
+                <div>
+                    <button onClick={this.decrement}>-</button>
+                    <button onClick={this.increment}>+</button>
+                </div>
             </section>
         );
+    }
+
+    increment() {
+        this.setState(state => ({
+            count: state.count + 1,
+            title: "INC"
+          }));
+    }
+
+    decrement() {
+        this.setState(state => ({
+            count: state.count - 1,
+            title: "DEC"
+          }));
     }
 }
 
